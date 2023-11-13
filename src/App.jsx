84041fcac1682007dea6428b23625a7d0a01b800
Pablo Redig <PabloRedigmrv@gmail.com>
2023-11-13 17:47:26 -0300

    Criado a função Deletar e Adicionar novo usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import {initializeApp }  from "firebase/app";
-import { collection, getFirestore, getDocs }  from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { useEffect, useState } from "react"
 
-const firebaseApp = initializeApp ( {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyCzWFls49ncmBVKD8XSNfNdJ9tDr41fL2w",
   authDomain: "react-firebase-dba81.firebaseapp.com",
   projectId: "react-firebase-dba81",
 });
-
 
 function App() {
 
@@ -15,34 +14,53 @@ function App() {
   const [email, setEmail] = useState("");
   const [users, setUsers] = useState([]);
 
-  const db = getFirestore (firebaseApp);
+  const db = getFirestore(firebaseApp);
 
-  const userCollectionRef = collection(db,"users")
+  const userCollectionRef = collection(db, "users")
 
+  // Adicionar novo user no BD
+  async function criarUser() {
+    const user = await addDoc(userCollectionRef, {
+      name,
+      email,
+    })
+  }
+
+  // Visualizar user no BD 
   useEffect(() => {
-    const getUsers = async () => {      
-      const data = await getDocs(userCollectionRef);       
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     };
     getUsers();
 
   }, [])
 
-  return (    
+  //Deletar User no BD
+  async function deleteUser(id) {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  }
+
+  return (
     <div>
+      <input type="text" placeholder="Nome" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <button onClick={criarUser}>Criar Users</button>
       <ul>
         {users.map((user) => {
-            return(
-              <div key={user.id}>
-                <li>{user.name} | {user.email}</li>               
-              </div>
-            )
-          })
+          return (
+            <div key={user.id}>
+              <li>{user.name} | {user.email}</li>
+              <button onClick={() => deleteUser(user.id)}>Deletar</button>
+            </div>
+          )
+        })
         }
       </ul>
     </div>
-    
-  
+
+
   );
 }
 

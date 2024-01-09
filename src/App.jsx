38f7207d8b7a385c99b54384eae46e281300fc95
Pablo Redig <PabloRedigmrv@gmail.com>
2024-01-09 20:35:48 -0300

    Ajuste de um Bug no informativo dos dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { collection, getFirestore, getDocs, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 import "./App.css";
 import Inserir from "./Modulos/InserirDados";
 import FiltroDeBusca from "./Modulos/FiltroDeBusca";
-import Card from "./Modulos/Card";
 
-//Conexao Banco de dados
+// Conexao Banco de dados
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyCzWFls49ncmBVKD8XSNfNdJ9tDr41fL2w",
   authDomain: "react-firebase-dba81.firebaseapp.com",
@@ -32,29 +39,59 @@ function EditUserForm({ user, onSave, onCancel }) {
 
   return (
     <>
-      {/* MODAL PARA EDITAR O PROTUDO */}
-
+      {/* MODAL PARA EDITAR O PRODUTO */}
       <div className="EditUserForm">
         <h2>Editar Produto</h2>
-        <label><strong>Nome</strong></label>
-        <input className="input" type="text" value={editedName} onChange={(e) => setEditedName(e.target.value)} />
+        <label>
+          <strong>Nome</strong>
+        </label>
+        <input
+          className="input"
+          type="text"
+          value={editedName}
+          onChange={(e) => setEditedName(e.target.value)}
+        />
 
-        <label><strong>Quantidade</strong></label>
-        <input className="input" type="text" value={editedQuantidade} onChange={(e) => setEditedQuantidade(e.target.value)} />
+        <label>
+          <strong>Quantidade</strong>
+        </label>
+        <input
+          className="input"
+          type="text"
+          value={editedQuantidade}
+          onChange={(e) => setEditedQuantidade(e.target.value)}
+        />
 
-        <label><strong>Tipo</strong></label>
-        <input className="input" type="text" value={editedTipo} onChange={(e) => setEditedTipo(e.target.value)} />
+        <label>
+          <strong>Tipo</strong>
+        </label>
+        <input
+          className="input"
+          type="text"
+          value={editedTipo}
+          onChange={(e) => setEditedTipo(e.target.value)}
+        />
 
-        <label><strong>Valor</strong></label>
-        <input className="input" type="text" value={editedValor} onChange={(e) => setEditedValor(e.target.value)} />
+        <label>
+          <strong>Valor</strong>
+        </label>
+        <input
+          className="input"
+          type="text"
+          value={editedValor}
+          onChange={(e) => setEditedValor(e.target.value)}
+        />
 
         <div className="acoes">
-          <button className="btn Green" onClick={handleSave}>Salvar</button>
-          <button className="btn Red" onClick={onCancel}>Cancelar</button>
+          <button className="btn Green" onClick={handleSave}>
+            Salvar
+          </button>
+          <button className="btn Red" onClick={onCancel}>
+            Cancelar
+          </button>
         </div>
       </div>
     </>
-
   );
 }
 
@@ -65,10 +102,10 @@ function App() {
   const [valor, setValor] = useState("");
   const [users, setUsers] = useState([]);
   const [forceUpdate, setForceUpdate] = useState(false);
-  const [searchTerm, setSearchTerm] = useState(""); // Novo estado para armazenar o tipo de busca
-  const [searchResults, setSearchResults] = useState([]); // Novo estado para armazenar os resultados da busca
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  //Const de Edição
+  // Const de Edição
   const [editUserId, setEditUserId] = useState(null);
   const [editedName, setEditedName] = useState("");
   const [editedQuantidade, setEditedQuantidade] = useState("");
@@ -85,7 +122,6 @@ function App() {
 
   const cancelEdit = () => {
     setEditUserId(null);
-    // Limpa os campos de edição
     setEditedName("");
     setEditedQuantidade("");
     setEditedTipo("");
@@ -93,22 +129,17 @@ function App() {
   };
 
   const saveEdit = async (editedUser) => {
-    // Realize a lógica de atualização no banco de dados
     const userDoc = doc(db, "users", editedUser.id);
     await updateDoc(userDoc, editedUser);
 
-    // Finaliza a edição
     setEditUserId(null);
-    // Limpa os campos de edição
     setEditedName("");
     setEditedQuantidade("");
     setEditedTipo("");
     setEditedValor("");
-    // Força a atualização da lista de usuários
     setForceUpdate((prev) => !prev);
   };
 
-  // Função para buscar usuários pelo tipo
   const searchByType = () => {
     const results = users.filter(
       (user) =>
@@ -118,11 +149,9 @@ function App() {
     setSearchResults(results);
   };
 
-  //Conexao Banco de dados
   const db = getFirestore(firebaseApp);
   const userCollectionRef = collection(db, "users");
 
-  // Adicionar novo user no BD
   async function criarUser() {
     if (!name || !quantidade || !tipo || !valor) {
       alert("Por favor, preencha todos os campos.");
@@ -143,7 +172,6 @@ function App() {
     setValor("");
   }
 
-  // Visualizar user no BD
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(userCollectionRef);
@@ -152,10 +180,11 @@ function App() {
     getUsers();
   }, [forceUpdate]);
 
-  //Deletar User no BD
   async function deleteUser(id) {
-    const confirmDelete = window.confirm("Tem certeza que deseja excluir este usuário?");
-    // Se o usuário clicar em "Cancelar", sai da função sem excluir o usuário
+    const confirmDelete = window.confirm(
+      "Tem certeza que deseja excluir este usuário?"
+    );
+
     if (!confirmDelete) {
       return;
     }
@@ -188,23 +217,51 @@ function App() {
       <div className="Box_Produto">
         {(searchTerm ? searchResults : users).map((user) => (
           <div key={user.id}>
-            <Card 
-              name={user.name}
-              quantidade={user.quantidade}
-              tipo={user.tipo}
-              valor={user.valor}
-              startEdit={startEdit}
-              user={user}
-              deleteUser={deleteUser}      
-            />
+            <div className="Card">
+              <h2>Produto</h2>
+              <div className="produto">
+                <span>
+                  <strong>NOME:</strong> {user.name}
+                </span>
+                <span>
+                  <strong>QUANTIDADE:</strong> {user.quantidade}
+                </span>
+                <span>
+                  <strong>TIPO:</strong> {user.tipo}
+                </span>
+                <span>
+                  <strong>VALOR:</strong> R${user.valor}
+                </span>
+              </div>
+
+              <div className="acoes">
+                <button className="btn" onClick={() => startEdit(user)}>
+                  Editar
+                </button>
+                <button
+                  className="btn Red"
+                  onClick={() => deleteUser(user.id)}
+                >
+                  Deletar
+                </button>
+              </div>
+
+              {editUserId === user.id && (
+                <EditUserForm
+                  user={user}
+                  onSave={(editedUser) => {
+                    saveEdit(editedUser);
+                    cancelEdit();
+                  }}
+                  onCancel={cancelEdit}
+                />
+              )}
+            </div>
           </div>
         ))}
       </div>
     </div>
-
   );
 }
 
 export default App;
-
-
